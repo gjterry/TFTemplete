4afb6df117f52630c17091cff3f2b0ea4a5bf696
terry.guo <terry.guo@edoctor.cn>
2014-02-25 14:02:14 +0800

    init project

---
+++ TFTemplete/Vendors/TFViewController.h
@@ -0,0 +1,13 @@
+//
+//  TFViewController.h
+//  TFTemplete
+//
+//  Created by Terry  on 14-2-25.
+//  Copyright (c) 2014年 Terry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFViewController : UIViewController
+
+@end
---
+++ TFTemplete/Vendors/TFViewController.m
@@ -0,0 +1,29 @@
+//
+//  TFViewController.m
+//  TFTemplete
+//
+//  Created by Terry  on 14-2-25.
+//  Copyright (c) 2014年 Terry. All rights reserved.
+//
+
+#import "TFViewController.h"
+
+@interface TFViewController ()
+
+@end
+
+@implementation TFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
